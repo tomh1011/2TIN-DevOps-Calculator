74da8b979e55565f2a7b52a835712a5cfc25f159
tomh1011 <43880446+tomh1011@users.noreply.github.com>
2020-11-30 11:57:46 +0100

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,6 +4,17 @@ class Calculator
     public function add($a, $b){
         return $a + $b;
     }
+    
+
+    public function subtract($a, $b){
+        return $a - $b;
+    }    
+    
+
+    public function multiply($a, $b){
+        return $a * $b;
+    }
+
 }
 
 ?>
